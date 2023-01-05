baeffc4e7018e077dbb19da60f1778f147a0d581
alex <alex@mail.com>
2023-01-05 21:55:56 +0300

    2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -5,7 +5,7 @@ let counter = 0;
 function sizeChange() {
     let clickTimer = Date.now();
     image.width = ++counter % 2 ? 250 : 200;
-    document.getElementById('clicker__counter').textContent = counter + '\n' + 'Скорость клика: ' + ((clickTimer - timer) / 1000).toFixed(2) + ' сек.';
+    document.getElementById('clicker__counter').textContent = counter + '\n' + 'Скорость клика: ' + (1 / ((clickTimer - timer) / 1000)).toFixed(2);
     timer = Date.now();
 }
 image.onclick = sizeChange;
