b6c33240b3c124288162b8f08738d2fd37a1b7a2
alex <alex@mail.com>
2023-01-12 22:24:59 +0300

    56

---
+++ dom/reveal/task.js
@@ -0,0 +1,15 @@
+const banners = document.querySelectorAll('.reveal');
+const viewPortSize = window.innerHeight;
+
+window.addEventListener('scroll', function() {   
+for (let item of banners) {
+    const ads = {tob, bottom} = item.getBoundingClientRect();
+    if ((viewPortSize - ads.top >= 0) && (ads.bottom >= 0)) { 
+        
+        item.classList.add('reveal_active');
+    }
+    else {
+        item.classList.remove('reveal_active');
+    }
+}
+});
